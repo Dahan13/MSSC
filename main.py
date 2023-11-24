618ceb291d3704ff8ad4929112aaa19ed84a8625
Dahan13 <corentincaugant2001@gmail.com>
2023-11-24 15:43:29 +0100

    Refactors main.py and strategies.py

--- main.py
+++ main.py
@@ -5,85 +5,132 @@ from strategies import *
 from classes.planning import *
 from classes.turbine import *
 from classes.team import *
+
 ############
 
-def weather_forecast(wind) :
-    r = random.random()
-    match wind :
-        case 1 :
-            if r < 0.2 :
-                return 1
-            elif r < 0.93 :
-                return 2
-            else :
-                return 3
-        case 2 :
-            if r < 0.11 :
-                return 1
-            elif r < 0.85 :
-                return 2
-            else :
-                return 3
-        case 3 :
-            if r < 0.04 :
-                return 1
-            elif r < 0.65 :
-                return 2
-            else :
-                return 3
 
-class System :
-    def __init__(self, N_turbines, N_teams) :
-        self.turbines = [Turbine(i) for i in range(1, N_turbines+1)]
-        self.teams = [Team(i) for i in range(1, N_teams+1)]
+def weather_forecast(wind):
+    """Returns the wind speed for the next day depending on the wind speed of the current day."""
+    r = random.random()
+    match wind:
+        case 1:
+            if r >= 0.2 and r < 0.93:
+                return 2
+            elif r >= 0.93:
+                return 3
+        case 2:
+            if r < 0.11:
+                return 1
+            elif r >= 0.85:
+                return 3
+        case 3:
+            if r < 0.04:
+                return 1
+            elif r < 0.65:
+                return 2
+    return wind
+
+
+class System:
+    def __init__(self, N_turbines, N_teams):
+        """
+        Creates the system with N_turbines turbines and N_teams teams.
+
+        Attributes:
+            turbines: list = list of all turbines
+            teams: list = list of all teams
+            planning: Planning = planning of all undergoing missions
+            wind: int = wind speed
+            days_count: int = number of days since the beginning of the simulation
+            total_prod: int = total energy produced since the beginning of the simulation
+            total_cost: int = total cost since the beginning of the simulation
+        """
+        self.turbines = [Turbine(i) for i in range(1, N_turbines + 1)]
+        self.teams = [Team(i) for i in range(1, N_teams + 1)]
         self.planning = Planning()
         self.wind = 1
         self.days_count = 0
         self.total_prod = 0
         self.total_cost = N_teams * 100000
-    def next_day(self, strategy) :
+
+    def next_day(self, strategy):
+        """Makes the system progress by a day by taking into account the strategy."""
         self.days_count += 1
-        if self.days_count % 365 == 0 :
+
+        # if it's a new year, we pay the maintenance teams yearly fee
+        if self.days_count % 365 == 0:
             self.total_cost += len(self.teams) * 100000
+
+        # We update the wind speed for the next day
         self.wind = weather_forecast(self.wind)
+
+        # We decide which missions to launch according to the strategy
         missions_to_launch = strategy(self)
-        for m in missions_to_launch :
+
+        # We launch the missions
+        for m in missions_to_launch:
             self.planning.new_mission(m[0], m[1])
-            for t in self.teams :
-                if t.get_id() == m[1] :
+
+            # We search & set the corresponding team in maintenance mode
+            for t in self.teams:
+                if t.get_id() == m[1]:
                     t.new_mission()
                     self.total_cost += 50000
-            for t in self.turbines :
-                if t.get_id() == m[0] :
+
+            # We search & set the corresponding turbine in maintenance mode
+            for t in self.turbines:
+                if t.get_id() == m[0]:
                     t.new_mission()
+
+        # We make the maintenance missions progress for the day
         update_info = self.planning.make_progress(self.wind)
-        turbines_repared = [u['turbine'] for u in update_info]
-        teams_availabled = [u['team'] for u in update_info]
-        for t in self.turbines :
-            if t.get_id() in turbines_repared :
+
+        # We retrive all turbines and teams that just finished their maintenance operation
+        turbines_repared = [u["turbine"] for u in update_info]
+        teams_availabled = [u["team"] for u in update_info]
+
+        # We update these turbines and teams to be available for the next day
+        for t in self.turbines:
+            if t.get_id() in turbines_repared:
                 t.repair()
                 t.end_mission()
-        for t in self.teams :
-            if t.get_id() in teams_availabled :
+        for t in self.teams:
+            if t.get_id() in teams_availabled:
                 t.end_mission()
-        for t in self.turbines :
-            if t.get_availability() :
+
+        # We make all the available turbines produce their energy for the day
+        for t in self.turbines:
+            if t.get_availability():
                 self.total_prod += t.produce(self.wind)
-        for t in self.turbines :
+
+        # We update all turbines states depending of the wind
+        for t in self.turbines:
             t.damage(self.wind)
-    def get_turbines(self) :
+
+    def get_turbines(self):
+        """Returns the list of all turbines."""
         return self.turbines
-    def get_teams(self) :
+
+    def get_teams(self):
+        """Returns the list of all teams."""
         return self.teams
-    def get_planning(self) :
+
+    def get_planning(self):
+        """Returns the mission planning."""
         return self.planning
-    def get_wind(self) :
+
+    def get_wind(self):
+        """Returns the wind speed."""
         return self.wind
-    def get_days_count(self) :
+
+    def get_days_count(self):
+        """Returns the number of days since the beginning of the simulation."""
         return self.days_count
-    def get_total_prod(self) :
+
+    def get_total_prod(self):
+        """Returns the total energy produced since the beginning of the simulation."""
         return self.total_prod
-    def get_total_cost(self) :
-        return self.total_cost 
-    def get_wind_memory(self) :
-        return self.wind_memory
+
+    def get_total_cost(self):
+        """Returns the total cost since the beginning of the simulation."""
+        return self.total_cost
--- strategies.py
+++ strategies.py
@@ -1,34 +1,51 @@
 ## IMPORT ##
 ############
 
-def strategy_application(system, condition) :
+
+from main import System
+
+
+def strategy_application(system: System, condition: function):
+    """Returns a list of new missions for a given system depending on the chosen conditions."""
     new_missions = []
+
+    # We begin by retrieving all available teams and functioning turbines
     available_teams = []
     available_turbines = []
-    for t in system.get_teams() :
-        if t.get_availability() :
+    for t in system.get_teams():
+        if t.get_availability():
             available_teams.append(t)
-    for t in system.get_turbines() :
-        if t.get_availability() :
+    for t in system.get_turbines():
+        if t.get_availability():
             available_turbines.append(t)
-    for t in available_turbines :
-        if len(available_teams) > 0 :
-            if condition(system, t) :
+
+    # Now for each turbine, we check if it meets the condition to start the maintenance and if there are still available teams
+    for t in available_turbines:
+        if len(available_teams) > 0:
+            if condition(system, t):
                 new_missions.append((t.get_id(), available_teams.pop().get_id()))
-        else :
+        else: # if there are no more available teams, we stop the loop
             break
     return new_missions
+
 
 """
             State   1   2   3   4   M
 Production
 0% - 100%           N   N   N   Y   N
 """
-def condition_A(system, turbine) :
+
+
+def condition_A(system, turbine):
+    """Returns True if the turbine is heavily damaged and False otherwise."""
     state = turbine.get_state()
     return state == 4
-def strategy_A(system) :
+
+
+def strategy_A(system):
+    """Returns a list of new missions for a given system depending on condition_A."""
     return strategy_application(system, condition_A)
+
 
 """
             State   1   2   3   4   M
@@ -37,9 +54,16 @@ Production
 70% - 90%           N   N   N   Y   N
 90% - 100%          N   N   N   N   N
 """
-def condition_B(system, turbine) :
+
+
+def condition_B(system, turbine):
+    """Returns True if the turbine is moderately damaged and operated for less than 70% of the time or if the turbine is heavily damaged and operated for less than 90% of the time.
+    It returns false otherwise"""
     state = turbine.get_state()
-    ratio = turbine.get_operating_days_count()/system.get_days_count()
+    ratio = turbine.get_operating_days_count() / system.get_days_count()
     return (state == 4 and ratio < 0.9) or (state == 3 and ratio < 0.7)
-def strategy_B(system) :
+
+
+def strategy_B(system):
+    """Returns a list of new missions for a given system depending on condition_B."""
     return strategy_application(system, condition_B)
